a307cedd6d06f552167482954431003072c77ba9
hawwwshim <166034398+hawwwshim@users.noreply.github.com>
2024-07-25 17:59:11 +0400

    Add files via upload

---
+++ Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CustomerApi.Models
+{
+    public class Customers
+    {
+        [Key]
+        public int CustomerId { get; set; }
+        public string? CustomerName { get; set; }
+        public string? ContactName { get; set; }
+        public string? Country { get; set; }
+    }
+}
